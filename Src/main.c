ea48cd17659974b1988fd8fabebe371f801941da
cvetaevvitaliy <cvetaevvitaliy@gmail.com>
2018-01-15 22:58:35 +0200

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -1,46 +1,9 @@
-/**
-  ******************************************************************************
-  * File Name          : main.c
-  * Description        : Main program body
-  ******************************************************************************
-  ** This notice applies to any and all portions of this file
-  * that are not between comment pairs USER CODE BEGIN and
-  * USER CODE END. Other portions of this file, whether 
-  * inserted by the user or by software development tools
-  * are owned by their respective copyright owners.
-  *
-  * COPYRIGHT(c) 2018 STMicroelectronics
-  *
-  * Redistribution and use in source and binary forms, with or without modification,
-  * are permitted provided that the following conditions are met:
-  *   1. Redistributions of source code must retain the above copyright notice,
-  *      this list of conditions and the following disclaimer.
-  *   2. Redistributions in binary form must reproduce the above copyright notice,
-  *      this list of conditions and the following disclaimer in the documentation
-  *      and/or other materials provided with the distribution.
-  *   3. Neither the name of STMicroelectronics nor the names of its contributors
-  *      may be used to endorse or promote products derived from this software
-  *      without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  *
-  ******************************************************************************
-  */
 
-/* Includes ------------------------------------------------------------------*/
+
 #include "main.h"
 #include "stm32f1xx_hal.h"
 
-/* USER CODE BEGIN Includes */
+
 #include "ssd1306.h"
 #include "fonts.h"
 #include "vape.h"
@@ -48,9 +11,7 @@
 #include <math.h>
 #include "eeprom.h"
 
-/* USER CODE END Includes */
 
-/* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
 DMA_HandleTypeDef hdma_adc1;
 
@@ -58,8 +19,7 @@ I2C_HandleTypeDef hi2c1;
 
 TIM_HandleTypeDef htim1;
 
-/* USER CODE BEGIN PV */
-/* Private variables ---------------------------------------------------------*/
+
 uint32_t adc_buffer[3];
 float value[3];
 float read_values[2];
@@ -111,9 +71,7 @@ uint32_t status_eeprom;
 uint32_t puffs=0;
 uint8_t low_batt=0;
 
-/* USER CODE END PV */
 
-/* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
@@ -124,8 +82,7 @@ static void MX_TIM1_Init(void);
 void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
                                 
 
-/* USER CODE BEGIN PFP */
-/* Private function prototypes -----------------------------------------------*/
+
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
 		if (hadc->Instance == ADC1)
@@ -135,48 +92,31 @@ void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 		}
 
 }
-/* USER CODE END PFP */
 
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
 
 int main(void)
 {
 
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration----------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  
   HAL_Init();
 
-  /* USER CODE BEGIN Init */
+
 	PWR->CSR   |= PWR_CSR_EWUP;
 	
- // HAL_PWR_PVDCallback();
-	//if ((PWR->CSR & PWR_CSR_WUF) == PWR_CSR_WUF) // ???? ?????, ?? ?????????? ????      
-    //     PWR->CR |= PWR_CR_CWUF;
-  /* USER CODE END Init */
 
-  /* Configure the system clock */
   SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+  
 	RCC->APB1ENR |= RCC_APB1ENR_PWREN;
 	
-  /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_ADC1_Init();
   MX_I2C1_Init();
   MX_TIM1_Init();
 
-  /* USER CODE BEGIN 2 */
+  
 	HAL_ADC_Start_DMA(&hadc1,adc_buffer,3);
 	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
 	ssd1306_Init();
@@ -205,15 +145,10 @@ int main(void)
 		
 		//tick_delay = HAL_GetTick();
 		
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+ 
   while (1)
   {
-  /* USER CODE END WHILE */
-
-  /* USER CODE BEGIN 3 */
+  
 		//tick_delay = HAL_GetTick();
 		Read_ADC();
 		temp_tik++;
@@ -388,7 +323,7 @@ int main(void)
 		
 
 
-  /* USER CODE END 3 */
+ 
 
 }
 
@@ -646,7 +581,7 @@ static void MX_GPIO_Init(void)
 
 }
 
-/* USER CODE BEGIN 4 */
+
 void Vout(void)
 {
 				
@@ -666,7 +601,7 @@ void Vout(void)
 
 
 
-/* USER CODE END 4 */
+
 
 /**
   * @brief  This function is executed in case of error occurrence.
@@ -675,12 +610,11 @@ void Vout(void)
   */
 void _Error_Handler(char * file, int line)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
+  
   while(1) 
   {
   }
-  /* USER CODE END Error_Handler_Debug */ 
+
 }
 
 #ifdef USE_FULL_ASSERT
